7011ea9c9592cd66efa428dc1ef2d24841fbfba3
vicoolchik <78905683+vicoolchik@users.noreply.github.com>
2022-11-24 11:29:56 +0200

    BusinessLogic Tests

--- BusinessLogic.Tests/AuthManagerTests.cs
+++ BusinessLogic.Tests/AuthManagerTests.cs
@@ -2,6 +2,9 @@
 using NUnit.Framework;
 using ProductCompany.BusinessLogic.Concrete;
 using ProductCompany.DAL.Interfaces;
+using ProductСompany.DTO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BusinessLogic.Tests
 {
@@ -27,6 +30,43 @@ namespace BusinessLogic.Tests
             var res = manager.Login(username, password);
 
             Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void IsManagerTest()
+        {
+            UserDTO user = new UserDTO(); 
+            userDal.Setup(d => d.IsManager(user)).Returns(true);
+            var res = manager.IsManager(user);
+
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void GetUsersTest()
+        {
+            List<UserDTO> users = new List<UserDTO>();
+            userDal.Setup(d => d.GetUsers()).Returns(users);
+            var res = manager.GetUsers();
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(users.Count(), res.Count());;
+        }
+
+        [Test]
+        public void GetUserByLoginTest()
+        {
+            string username = "user";
+            UserDTO users = new UserDTO
+            {
+                UserID = 1,
+                Login = username
+            };
+            userDal.Setup(d => d.GetUserByLogin(username)).Returns(users);
+            var res = manager.GetUserByLogin(username);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(users.UserID, res.UserID); ;
         }
     }
 }
--- BusinessLogic.Tests/ProductCompanyManagerTests.cs
+++ BusinessLogic.Tests/ProductCompanyManagerTests.cs
@@ -4,6 +4,8 @@ using ProductCompany.BusinessLogic.Concrete;
 using ProductCompany.BusinessLogic.Interfaces;
 using ProductCompany.DAL.Interfaces;
 using ProductСompany.DTO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BusinessLogic.Tests
 {
@@ -22,6 +24,7 @@ namespace BusinessLogic.Tests
             productDal = new Mock<IProductDal>(MockBehavior.Strict);
             categoryDal = new Mock<ICategoryDal>(MockBehavior.Strict);
             supplierDal = new Mock<ISupplierDal>(MockBehavior.Strict);
+            authManager = new Mock<IAuthManager>(MockBehavior.Strict);
 
             manager = new ProductCompanyManager(categoryDal.Object, productDal.Object, supplierDal.Object, authManager.Object);
         }
@@ -47,6 +50,162 @@ namespace BusinessLogic.Tests
 
             Assert.IsNotNull(res);
             Assert.AreEqual(outProduct.ProductID, res.ProductID);
+        }
+
+        [Test]
+        public void DeleteProductByIDTest()
+        {
+            ProductDTO inProduct = new ProductDTO
+            {
+                ProductName = "Test",
+                ProductID = 1,
+                SupplierID = 1,
+                CategoryID = 1,
+                UnitPrice = 0,
+                UnitsInStock = 0,
+                UnitsOnOnder = 0,
+                Discontinued = true
+            };
+            ProductDTO outProduct = new ProductDTO { ProductID = 1 };
+
+            productDal.Setup(d => d.DeleteProductByID(inProduct.ProductID)).Returns(outProduct);
+            var res = manager.DeleteProductByID(inProduct.ProductID);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outProduct.ProductID, res.ProductID);
+        }
+
+        [Test]
+        public void UpdateProductByIDTest()
+        {
+            ProductDTO inProduct = new ProductDTO
+            {
+                ProductName = "Test",
+                ProductID = 1,
+                SupplierID = 1,
+                CategoryID = 1,
+                UnitPrice = 0,
+                UnitsInStock = 0,
+                UnitsOnOnder = 0,
+                Discontinued = true
+            };
+            ProductDTO outProduct = new ProductDTO { ProductID = 1 };
+
+            productDal.Setup(d => d.DeleteProductByID(inProduct.ProductID)).Returns(outProduct);
+            var res = manager.DeleteProductByID(inProduct.ProductID);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outProduct.ProductID, res.ProductID);
+        }
+
+        [Test]
+        public void BlockProductByIDTest()
+        {
+            ProductDTO inProduct = new ProductDTO
+            {
+                ProductName = "Test",
+                ProductID = 1,
+                SupplierID = 1,
+                CategoryID = 1,
+                UnitPrice = 0,
+                UnitsInStock = 0,
+                UnitsOnOnder = 0,
+                Discontinued = true
+            };
+            ProductDTO outProduct = new ProductDTO { ProductID = 1 };
+
+            productDal.Setup(d => d.BlockProductByID(inProduct.ProductID)).Returns(outProduct);
+            var res = manager.BlockProductByID(inProduct.ProductID);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outProduct.ProductID, res.ProductID);
+        }
+
+        [Test]
+        public void GetListOfProductsTest()
+        {
+            List<ProductDTO> products = new List<ProductDTO>();
+            productDal.Setup(d => d.GetAllProducts()).Returns(products);
+            var res = manager.GetListOfProducts();
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(products.Count(), res.Count()); ;
+        }
+
+        [Test]
+        public void GetListOfProductsByCategoryIDTest()
+        {
+            int categoryId = 1;
+            List<ProductDTO> products = new List<ProductDTO>();
+            productDal.Setup(d => d.GetAllProductsByCategoryID(categoryId)).Returns(products);
+            var res = manager.GetListOfProductsByCategoryID(categoryId);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(products.Count(), res.Count()); ;
+        }
+
+        [Test]
+        public void AddCategoryTest()
+        {
+            CategoryDTO inCategory = new CategoryDTO
+            {
+                CategoryName = "Test",
+                CategoryID = 1
+            };
+            CategoryDTO outCategory = new CategoryDTO { CategoryID = 1 };
+
+            categoryDal.Setup(d => d.CreateCategory(inCategory)).Returns(outCategory);
+            var res = manager.AddCategory(inCategory);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outCategory.CategoryID, res.CategoryID);
+        }
+
+
+
+        [Test]
+        public void DeleteCategoryByIDTest()
+        {
+            CategoryDTO inCategory = new CategoryDTO
+            {
+                CategoryName = "Test",
+                CategoryID = 1
+            };
+            CategoryDTO outCategory = new CategoryDTO { CategoryID = 1 };
+
+            categoryDal.Setup(d => d.DeleteCategoryByID(inCategory.CategoryID)).Returns(outCategory);
+            var res = manager.DeleteCategoryByID(inCategory.CategoryID);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outCategory.CategoryID, res.CategoryID);
+        }
+
+        [Test]
+        public void GetListOfCategoriesTest()
+        {
+            List<CategoryDTO> categories = new List<CategoryDTO>();
+            categoryDal.Setup(d => d.GetAllCategories()).Returns(categories);
+            var res = manager.GetListOfCategories();
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(categories.Count(), res.Count()); ;
+        }
+
+        [Test]
+        public void UpdateCategoryyByIDTest()
+        {
+            CategoryDTO inCategory = new CategoryDTO
+            {
+                CategoryName = "Test",
+                CategoryID = 1
+            };
+            CategoryDTO outCategory = new CategoryDTO { CategoryID = 1 };
+
+            categoryDal.Setup(d => d.EditCategoryyByID(inCategory, inCategory.CategoryID)).Returns(outCategory);
+            var res = manager.UpdateCategoryyByID(inCategory, inCategory.CategoryID);
+
+            Assert.IsNotNull(res);
+            Assert.AreEqual(outCategory.CategoryID, res.CategoryID);
         }
     }
 }
